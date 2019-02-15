56b6580f4e1abe8f85d5e7a50f72e0c5faa39071
okunnigatosin <okunnigatosin@gmail.com>
2019-02-15 14:58:00 +0100

    child expressions and elements

---
+++ src/AuthorQuiz.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './AuthorQuiz';
+import AuthorQuiz from './AuthorQuiz';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AuthorQuiz />, div);
+  //ReactDOM.unmountComponentAtNode(div);
+});
